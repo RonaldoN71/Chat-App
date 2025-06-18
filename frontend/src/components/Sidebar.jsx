a05ac679fe3bf5d6780a3f0fc9445f4936abe5b1
RonaldoN71 <ronaldonandeibam7@gmail.com>
2025-06-18 12:35:21 +0530

    changes of mobile screen

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -19,8 +19,8 @@ function Sidebar() {
   if (isUsersLoading) return <SidebarSkeleton />;
 
   return (
-    <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
-        <div className="border-b border-base-300 w-full p-5">
+    <aside className="h-full w-full lg:w-72  flex flex-col  transition-all duration-200">
+        <div className="border-b border-base-400 w-full p-5">
                 <div className="flex items-center gap-2">
              <Users className="size-6" />
               <span className="font-medium hidden lg:block">Contacts</span>
@@ -39,17 +39,17 @@ function Sidebar() {
                 <span className="text-xs text-zinc-500">({onlineUsers.length>0 ?onlineUsers.length-1:0} online)</span>
               </div>
            </div>
-        <div className="overflow-y-auto w-full py-3">
+        <div className="overflow-y-auto w-full py-3 ">
             {filteredUsers.map((user)=>(
                 <button
                 key={user._id} onClick={()=>setSelectedUser(user)}
                 className= {`
               w-full p-3 flex items-center gap-3
-              hover:bg-base-300 transition-colors
+              hover:bg-base-300 transition-colors 
               ${selectedUser?._id === user._id ? "bg-base-300 ring-1 ring-base-300" : ""}
             `}
                 >
-                    <div className="relative mx-auto lg:mx-0">
+                    <div className="relative mx-0">
                     <img
                     src={user.profilePic || "/avatar.png"}
                     alt={user.name}
@@ -61,7 +61,7 @@ function Sidebar() {
                     </div>
 
                     {/* User info - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
+            <div className=" lg:block text-left min-w-0">
               <div className="font-medium truncate">{user.fullName}</div>
               <div className="text-sm text-zinc-400">
                 {onlineUsers.includes(user._id) ? "Online" : "Offline"}
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,20 +1,27 @@
 import { useChatStore } from "../store/useChatStore";
-
+import {useState,useEffect} from 'react';
 import Sidebar from "../components/Sidebar";
 import NoChatSelected from "../components/NoChatSelected";
 import ChatContainer from "../components/ChatContainer";
 
 const HomePage = () => {
   const { selectedUser } = useChatStore();
-
+  const [isMobile,setIsMobile] = useState(window.innerWidth<768);
+  useEffect(()=>{
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize",handleResize);
+    return ()=> window.removeEventListener("resize",handleResize);
+  },[])
   return (
     <div className="h-screen bg-base-200">
       <div className="flex items-center justify-center pt-20 px-4">
         <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
-          <div className="flex h-full rounded-lg overflow-hidden">
+          <div className="flex h-full rounded-lg ">
+          { isMobile ? (!selectedUser ? <Sidebar />: <ChatContainer/>) :
+           ( <>
             <Sidebar />
-
             {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+            </>)}
           </div>
         </div>
       </div>
